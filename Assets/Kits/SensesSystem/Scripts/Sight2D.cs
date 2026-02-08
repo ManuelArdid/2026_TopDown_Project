d85b2ba692d7dbb3b691fc75a67cc8ca79055963
ManuelArdid <manuelardid0@gmail.com>
2026-02-08 12:50:24 +0100

    feat(sight2D): support multiple targets and select closest one

--- Assets/Kits/SensesSystem/Scripts/Sight2D.cs
+++ Assets/Kits/SensesSystem/Scripts/Sight2D.cs
@@ -6,7 +6,10 @@ public class Sight2D : MonoBehaviour
     //[SerializeField] float checkFrecuency = 5f;
 
 
-    float lastCheckTime;
+    private float lastCheckTime;
+    private Transform _closestPlayer;
+    private float _distanceToClosestPlayer;
+
     Collider2D[] colliders;
     // Update is called once per frame
     void Update()
@@ -15,25 +18,41 @@ public class Sight2D : MonoBehaviour
         {
             lastCheckTime = Time.time;
         }
-        
+
         colliders = Physics2D.OverlapCircleAll(transform.position, radius);
 
+        _distanceToClosestPlayer = Mathf.Infinity;
+        _closestPlayer = null;
         for (int i = 0; i < colliders.Length; i++)
         {
+            if (colliders[i].CompareTag("Player"))
+            {
+                float distanceToPlayer = Vector2.Distance(transform.position, colliders[i].transform.position);
+                if (distanceToPlayer < _distanceToClosestPlayer)
+                {
+                    _distanceToClosestPlayer = distanceToPlayer;
+                    _closestPlayer = colliders[i].transform;
+                }
+            }
             Debug.Log($"El collider {i} se llama {colliders[i].name}.", colliders[i]);
         }
+    }
+
+    public Transform GetClosestTarget()
+    {
+        return _closestPlayer;
     }
 
     public bool IsPlayerInSight()
     {
         bool isPlayerInSight = false;
-        
+
         colliders = Physics2D.OverlapCircleAll(transform.position, radius);
 
         for (int i = 0; !isPlayerInSight && (i < colliders.Length); i++)
         {
-           if(colliders[i].CompareTag("Player"))
-            { isPlayerInSight = true;}
+            if (colliders[i].CompareTag("Player"))
+            { isPlayerInSight = true; }
         }
         return isPlayerInSight;
     }
